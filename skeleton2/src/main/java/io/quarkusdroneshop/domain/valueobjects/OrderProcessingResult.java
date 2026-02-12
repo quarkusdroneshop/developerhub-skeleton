06426bc1472e908755d66a62b42d16e220e7633b
Noriaki Mushino <nmushino@redhat.com>
2026-02-12 16:26:56 +0900

    delete template2

--- skeleton2/src/main/java/io/quarkusdroneshop/domain/valueobjects/OrderProcessingResult.java
+++
@@ -1,7 +0,0 @@
-package io.quarkusdroneshop.domain.valueobjects;
-
-public interface OrderProcessingResult {
-    boolean isEightySixed();
-    OrderUp getOrderUp();
-    RewardEvent getRewardEvent();
-}
