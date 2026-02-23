e936b240d43d156f90b5eb366f46fcf8d8094e64
Noriaki Mushino <nmushino@redhat.com>
2026-02-23 16:14:12 +0900

    update tekton

--- developerhub/plugins/tekton.ts
+++ developerhub/plugins/tekton.ts
@@ -1,13 +1,10 @@
 // packages/backend/src/plugins/tekton.ts
-import { tektonActions } from '@janus-idp/backstage-plugin-tekton';
-import { createRouter } from '@backstage/plugin-scaffolder-backend';
-import { Router } from 'express';
+import { createRouter as createScaffolderRouter } from '@backstage/plugin-scaffolder-backend';
+import { tektonActions } from '@backstage-community/plugin-tekton-backend';
+import type { Router } from 'express';
 
-export default async function createTektonPlugin(env: {
-  logger: any;
-  database: any;
-}): Promise<Router> {
-  return await createRouter({
+export default async function createTektonPlugin(env: { logger: any; database: any; }): Promise<Router> {
+  return await createScaffolderRouter({
     logger: env.logger,
     database: env.database,
     actions: [...tektonActions],
