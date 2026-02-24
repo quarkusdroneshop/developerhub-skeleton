aa6d805be29cde271cafddfdf164a1969b047cfd
Noriaki Mushino <nmushino@redhat.com>
2026-02-24 19:13:05 +0900

    Update template

--- developerhub/packages/backend/src/index.ts
+++ developerhub/packages/backend/src/index.ts
@@ -1,11 +1,3 @@
-/*
- * Hi!
- *
- * Note that this is an EXAMPLE Backstage backend. Please check the README.
- *
- * Happy hacking!
- */
-
 import { createBackend } from '@backstage/backend-defaults';
 import { createHttpRequestAction } from '@roadiehq/scaffolder-backend-module-http-request';
 
@@ -15,78 +7,24 @@ async function main() {
   backend.add(import('@backstage/plugin-app-backend'));
   backend.add(import('@backstage/plugin-proxy-backend'));
 
-  // scaffolder plugin
   backend.add(import('@backstage/plugin-scaffolder-backend'));
   backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
-  backend.add(
-    import('@backstage/plugin-scaffolder-backend-module-notifications'),
-  );
+
   backend.add(
     import('@roadiehq/scaffolder-backend-module-http-request'),
   );
 
-  // ✅ 正しい追加方法
-  backend.add({
-    pluginId: 'scaffolder',
-    moduleId: 'http-request-module',
-    register(env) {
-      env.registerInit({
-        deps: {
-          scaffolder: scaffolderActionsExtensionPoint,
-        },
-        async init({ scaffolder }) {
-          scaffolder.addActions(createHttpRequestAction());
-        },
-      });
-    },
-  });
-
-  // techdocs plugin
   backend.add(import('@backstage/plugin-techdocs-backend'));
-
-  // auth plugin
   backend.add(import('@backstage/plugin-auth-backend'));
-  // See https://backstage.io/docs/backend-system/building-backends/migrating#the-auth-plugin
   backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
-  // See https://backstage.io/docs/auth/guest/provider
-
-  // catalog plugin
   backend.add(import('@backstage/plugin-catalog-backend'));
-  backend.add(
-    import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
-  );
-
-  // See https://backstage.io/docs/features/software-catalog/configuration#subscribing-to-catalog-errors
-  backend.add(import('@backstage/plugin-catalog-backend-module-logs'));
-
-  // permission plugin
   backend.add(import('@backstage/plugin-permission-backend'));
-  // See https://backstage.io/docs/permissions/getting-started for how to create your own permission policy
-  backend.add(
-    import('@backstage/plugin-permission-backend-module-allow-all-policy'),
-  );
-
-  // search plugin
   backend.add(import('@backstage/plugin-search-backend'));
-
-  // search engine
-  // See https://backstage.io/docs/features/search/search-engines
-  backend.add(import('@backstage/plugin-search-backend-module-pg'));
-
-  // search collators
-  backend.add(import('@backstage/plugin-search-backend-module-catalog'));
-  backend.add(import('@backstage/plugin-search-backend-module-techdocs'));
-
-  // kubernetes plugin
   backend.add(import('@backstage/plugin-kubernetes-backend'));
-
-  // notifications and signals plugins
   backend.add(import('@backstage/plugin-notifications-backend'));
   backend.add(import('@backstage/plugin-signals-backend'));
 
-  backend.start();
-
-await backend.start();
+  await backend.start();
 }
 
 main().catch(err => {
