63485a6f7b1d4981af87513e6ca81b0867c6defb
Noriaki Mushino <nmushino@redhat.com>
2026-06-12 09:31:02 +0900

    Once deleted.

--- developerhub/dynamic-plugins/kubernetes-scaffolder-module/src/index.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const kubernetesScaffolderModule: import("@backstage/backend-plugin-api").BackendFeature;
--- developerhub/dynamic-plugins/test/kubernetes-scaffolder-module/index.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const createPipelineRunAction: () => import("@backstage/plugin-scaffolder-node").TemplateAction<{
-    manifest: string;
-    namespace: string;
-}, {
-    [x: string]: any;
-}, "v2">;
